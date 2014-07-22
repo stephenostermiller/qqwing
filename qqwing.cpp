29d8460761616ca416343445c733aa72bc9eccff
Stephen Ostermiller <http://ostermiller.org/contact.pl>
2014-07-22 17:18:41 -0400

    Add missing copyright

--- qqwing.cpp
+++ qqwing.cpp
@@ -3,6 +3,7 @@
  * Copyright (C) 2006-2011 Stephen Ostermiller
  * http://ostermiller.org/qqwing/
  * Copyright (C) 2007 Jacques Bensimon (jacques@ipm.com)
+ * Copyright (C) 2011 Jean Guillerez (j.guillerez - orange.fr)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
