8872349a70429d3fbd1dfdf24588290bdb10cd99
Stephen Ostermiller <http://ostermiller.org/contact.pl>
2014-08-04 05:56:45 -0400

    Unit tests

---
+++ build/test-run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
+
+version=`build/version.sh`
+
+case $1 in
+    "java")
+        export QQWING="java -jar target/qqwing-$version.jar"
+        ;;
+    "cpp")
+        export QQWING="target/qqwing"
+        ;;
+    *)
+        echo "Expected java or cpp as argument"
+        exit 1
+        ;;
+esac
+
+for test in test/*.sh
+do
+    $test
+done
