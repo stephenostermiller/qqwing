9c01dca1ddc7862e9ea1e056b44675c2dd9f606c
Stephen Ostermiller <http://ostermiller.org/contact.pl>
2014-08-04 06:07:19 -0400

    White space

--- build/test-run.sh
+++ build/test-run.sh
@@ -6,19 +6,19 @@ set -o pipefail
 version=`build/version.sh`
 
 case $1 in
-    "java")
-        export QQWING="java -jar target/qqwing-$version.jar"
-        ;;
-    "cpp")
-        export QQWING="target/qqwing"
-        ;;
-    *)
-        echo "Expected java or cpp as argument"
-        exit 1
-        ;;
+	"java")
+		export QQWING="java -jar target/qqwing-$version.jar"
+		;;
+	"cpp")
+		export QQWING="target/qqwing"
+		;;
+	*)
+		echo "Expected java or cpp as argument"
+		exit 1
+		;;
 esac
 
 for test in test/*.sh
 do
-    $test
+	$test
 done
