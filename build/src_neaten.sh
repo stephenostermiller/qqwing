491cebfa7d0d5ab65ce04d4855125e8b6a55310e
Stephen Ostermiller <http://ostermiller.org/contact.pl>
2014-09-18 14:59:37 -0400

    Don't try to neaten gif images

--- build/src_neaten.sh
+++ build/src_neaten.sh
@@ -26,6 +26,6 @@ then
 	touch --date 1980-01-01 $neaten_time_file
 fi
 
-find build/ doc/ src/ test/ Makefile -type f -newer $neaten_time_file | grep -vEi 'changelog|debian-copyright|COPYING|README' | xargs ./build/src_neaten.pl
+find build/ doc/ src/ test/ Makefile -type f -newer $neaten_time_file | grep -vEi 'changelog|debian-copyright|COPYING|README|\.gif$' | xargs ./build/src_neaten.pl
 
 touch $neaten_time_file
